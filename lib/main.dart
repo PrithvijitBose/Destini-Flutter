ea148dc1c8b7a11ae8fcb3940b7b22e6bee99c81
PrithvijitBose <prithvijitb18@gmail.com>
2024-07-10 11:06:44 +0530

    Make your choices

---
+++ lib/main.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'story_brain.dart';
+
+//TODO: Step 15 - Run the app and see if you can see the screen update with the first story. Delete this TODO if it looks as you expected.
+
+void main() => runApp(Destini());
+
+class Destini extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: StoryPage(),
+    );
+  }
+}
+int StoryNumber = 0;
+
+
+
+StoryBrain storyBrain = StoryBrain();
+
+class StoryPage extends StatefulWidget {
+  _StoryPageState createState() => _StoryPageState();
+}
+
+class _StoryPageState extends State<StoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     body: Container(
+  decoration: BoxDecoration(
+    image: DecorationImage(
+      image: AssetImage('images/background.png'),
+      fit: BoxFit.cover,
+    ),
+  ),
+  padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
+  constraints: BoxConstraints.expand(),
+  child: SafeArea(
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 12,
+          child: Center(
+            child: Text(
+              storyBrain.getStory(),
+              style: TextStyle(
+                fontSize: 25.0,
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 2,
+          child: TextButton(
+            onPressed: () {
+              storyBrain.nextStory(1);
+
+              setState(() {
+              storyBrain.nextStory(1);
+              });
+            
+            },
+            style: TextButton.styleFrom(
+              backgroundColor: Colors.red,
+              foregroundColor: Colors.white,
+            ),
+            child: Text(
+             
+              storyBrain.getChoice1(),
+              style: TextStyle(
+                fontSize: 20.0,
+              ),
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 20.0,
+        ),
+        Expanded(
+          flex: 2,
+          // TODO: Step 26 - Use a Flutter Visibility Widget to wrap this TextButton.
+          // TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
+          child: TextButton(
+            onPressed: () {
+        storyBrain.nextStory(2);
+        setState(() {
+          storyBrain.nextStory(2);
+        });
+            },
+            style: TextButton.styleFrom(
+              backgroundColor: Colors.blue,
+              foregroundColor: Colors.white,
+            ),
+            child: Text(
+             
+              storyBrain.getChoice2(),
+              style: TextStyle(
+                fontSize: 20.0,
+              ),
+            ),
+          ),
+        ),
+      ],
+    ),
+  ),
+));
+  }}
+
+//TODO: Step 24 - Run the app and try to figure out what code you need to add to this file to make the story change when you press on the choice buttons.
+
+//TODO: Step 29 - Run the app and test it against the Story Outline to make sure you've completed all the steps. The code for the completed app can be found here: https://github.com/londonappbrewery/destini-challenge-completed/
